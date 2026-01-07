51c7b3d23afe3a05959539688cf1eec61ccd2c4b
Zerx Developments <155743571+ZerxDevelopments@users.noreply.github.com>
2026-01-07 12:11:12 +0000

    Refactor pawnshop NPC configuration and items

--- config.lua
+++ config.lua
@@ -2,22 +2,10 @@ Config = {}
 
 -- Pawnshop NPC location
 Config.PawnLocation = {
-    coords = vector4(450.96, -1470.68, 28.29, 112.93), -- x, y, z, heading
+    coords = vector4(438.86, -1466.09, 28.29, 71.37), -- x, y, z, heading
     ped = 's_m_m_highsec_01', -- NPC model
 }
 
-Config.PawnLocation = {
-    [1] = {
-            coords = vector3(449.18, -1471.93, 29.29),
-            length = 1.5,
-            width = 1.8,
-            heading = 207.0,
-            debugPoly = false,
-            minZ = 100.97,
-            maxZ = 105.42,
-            distance = 3.0
-        },
-    }
 
 -- Items you allow players to sell
 Config.SellableItems = {
@@ -34,6 +22,3 @@ Config.SellableItems = {
 
 -- Minimum amount to sell
 Config.MinSellAmount = 1
-
--- Interaction text
-Config.InteractText = "[E] Sell Items"
